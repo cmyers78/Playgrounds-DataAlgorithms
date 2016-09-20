88ba3fc025a8427594412fa722d7b2ccf2f83bec
Chris Myers <cmyers78@gmail.com>
2016-09-19 21:50:06 -0400

    Greedy method isn't working.

--- Data Structure PlayGrounds/Product of all other numbers.playground/Contents.swift
+++ Data Structure PlayGrounds/Product of all other numbers.playground/Contents.swift
@@ -16,11 +16,11 @@ let testArray = [1, 7, 3, 4]
 func loopArray(array : [Int]) -> [Int] {
     var newArray = [Int]()
     
-    for outerIndex in testArray {
+    for outerIndex in array {
         //print(outerIndex)
         var product = 1
         
-        for innerIndex in testArray {
+        for innerIndex in array {
             if innerIndex != outerIndex {
                 product = product * innerIndex
                 
@@ -34,35 +34,32 @@ func loopArray(array : [Int]) -> [Int] {
 
 loopArray(testArray)
 
-//var newArray = [Int]()
-//
-//for outerIndex in testArray {
-//    //print(outerIndex)
-//    var product = 1
-//    
-//    for innerIndex in testArray {
-//        if innerIndex != outerIndex {
-//            product = product * innerIndex
-//
-//            
-//        }
-//    }
-//    newArray.append(product)
-//}
-//print(newArray)
-// output should read: [84, 12, 28, 21]
+// this isn't working....
 
-func getProductsOfIntsExceptIndex(array : [Int]) {
+func getProductsOfIntsExceptIndex(array : [Int]) -> [Int] {
     var productsOfAllIntsExceptAtIndex = [Int(array.count)]
     
     var productThusFar = 1
     var counter = 0
     while counter < array.count {
         productsOfAllIntsExceptAtIndex[counter] = productThusFar
-        productThusFar = productThusFar * array[counter]
-        counter = counter + 1
+        productThusFar  *= array[counter]
+        counter += 1
     }
+    
+    productThusFar = 1
+    counter = array.count - 1
+    while counter >= 0 {
+        productsOfAllIntsExceptAtIndex[counter] *= productThusFar
+        productThusFar = productThusFar * array[counter]
+        counter = counter - 1
+        
+    }
+    
+    return productsOfAllIntsExceptAtIndex
 }
+
+//getProductsOfIntsExceptIndex(testArray)
 
 
 
